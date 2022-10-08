2a2b3507f0d855ec3145db94486dc06fc693eba5
Sanay Shah <sanayshah2@gmail.com>
2022-10-08 19:13:53 -0400

    Update views.py

--- grievancesystemlatest/grievance/views.py
+++ grievancesystemlatest/grievance/views.py
@@ -7,10 +7,6 @@ from .decorators import *
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group,User
-
-
-
-
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
