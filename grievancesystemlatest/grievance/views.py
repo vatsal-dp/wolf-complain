f9445deb407ad8412eeb39d5c26ba174d20f174c
Sanay Shah <sanayshah2@gmail.com>
2022-10-08 19:13:37 -0400

    Update views.py

--- grievancesystemlatest/grievance/views.py
+++ grievancesystemlatest/grievance/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render,redirect,reverse,HttpResponse
 from.models import Complain,Admin,Student
 from.forms import *
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import Group,User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from .decorators import *
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group,User
+
 
 
 
