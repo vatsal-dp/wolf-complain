58c34efbb28cf434960fbd2155f0301f312fe5e1
Sanay Shah <sanayshah2@gmail.com>
2022-10-09 14:16:11 -0400

    Structure changes

--- grievancesystemlatest/grievance/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GrievanceConfig(AppConfig):
-    name = 'grievance'
--- grievancesystemlatest/grievance/tokens.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.tokens import PasswordResetTokenGenerator
-
-
-class TokenGenerator(PasswordResetTokenGenerator):
-    pass
-
-account_activation_token = TokenGenerator()
