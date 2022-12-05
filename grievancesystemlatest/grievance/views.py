c585f114fefa819493a28c434844ceae32269c63
divyagiridhar <30555592+divyagiridhar@users.noreply.github.com>
2022-12-04 22:47:10 -0500

    Update views.py

--- grievancesystemlatest/grievance/views.py
+++ grievancesystemlatest/grievance/views.py
@@ -20,3 +20,49 @@ def likecomplain(request):
     else:
         complain.likes.add(request.user)
     return redirect('collegefeed')
+
+# Create your views here.
+def home(request):
+   # group=Group.objects.get(user=request.user)
+
+
+    return render(request,'grievance/home.html')
+
+@is_logged
+def adminRegister(request):
+    if request.method=='POST':
+        form=UserFormAdmin(request.POST)
+        if form.is_valid():
+            user=form.save(commit=False)
+            name = user.username
+            user.is_active = True
+            email = form.cleaned_data.get('email')
+            first_name = form.cleaned_data.get('first_name')
+            for x in User.objects.all():
+                if x.email == email:
+                    messages.info(request, f'Account with this email already exists.')
+                    return redirect('adminRegister')
+            user.save()
+            group=Group.objects.get(name='faculty')
+            user.groups.add(group)
+            current_site = get_current_site(request)                #Email activation
+            mail_subject = 'Activate your admin account.'
+            message = render_to_string('grievance/acc_adminactive_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':account_activation_token.make_token(user),
+                'name':name
+            })
+            to_email = email
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            messages.info(request, 'Please confirm your email address to complete the registration')   #Email activation end
+            return redirect('loginAdmin')
+    else:
+        form=UserFormAdmin()
+    return render(request,'grievance/registeradmin.html',{'form':form})
+
+
